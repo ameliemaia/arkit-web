ac9f7eedbc9d17d881043ee2afb0faccee79fa22
Amelie Rosser <amelierosser1986@gmail.com>
2018-10-13 16:37:59 -0400

    Adding fullscreen web view for iPhone X

---
+++ ARKitWeb/FullscreenWKWebView.swift
@@ -0,0 +1,19 @@
+//
+//  FullscreenWKWebView.swift
+//  KikkAR
+//
+//  Created by Amelie Rosser on 06/10/2018.
+//  Copyright © 2018 KIKK. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+// https://stackoverflow.com/questions/47244002/make-wkwebview-real-fullscreen-on-iphone-x-remove-safe-area-from-wkwebview
+
+class FullScreenWKWebView: WKWebView {
+    override var safeAreaInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
